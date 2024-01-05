be5e4d5903d2dac052d82a218ef67c1aa5089236
Alex Kotov <kotovalexarian@gmail.com>
2024-01-06 00:51:35 +0400

    Improve the list of files

--- files.sh
+++ files.sh
@@ -1,52 +1,52 @@
-install_file root root    644 '/etc/htoprc'                               common
-install_file root root    644 '/etc/tmux.conf'                            common
+install_file common root root    644 '/etc/htoprc'
+install_file common root root    644 '/etc/tmux.conf'
 echo
-install_dir  root root    755 '/etc/vim'
-install_file root root    644 '/etc/vim/vimrc.local'                      common
+install_dir         root root    755 '/etc/vim'
+install_file common root root    644 '/etc/vim/vimrc.local'
 echo
-install_dir  root root    755 '/etc/fish'
-install_dir  root root    755 '/etc/fish/conf.d'
-install_dir  root root    755 '/etc/fish/functions'
-install_file root root    644 '/etc/fish/config.fish'                     common
-install_file root root    644 '/etc/fish/conf.d/rvm.fish'                 common
-install_file root root    644 '/etc/fish/functions/fish_prompt.fish'      common
-install_file root root    644 '/etc/fish/functions/prompt_user_host.fish' common
-install_file root root    644 '/etc/fish/functions/rvm.fish'              common
+install_dir         root root    755 '/etc/fish'
+install_dir         root root    755 '/etc/fish/conf.d'
+install_dir         root root    755 '/etc/fish/functions'
+install_file common root root    644 '/etc/fish/config.fish'
+install_file common root root    644 '/etc/fish/conf.d/rvm.fish'
+install_file common root root    644 '/etc/fish/functions/fish_prompt.fish'
+install_file common root root    644 '/etc/fish/functions/prompt_user_host.fish'
+install_file common root root    644 '/etc/fish/functions/rvm.fish'
 
 if [ "$PREFIX" = 'gentoo' ]; then
 echo
-install_file root root    644 '/etc/ccache.conf'                          gentoo
-install_file root root    644 '/etc/dracut.conf'                          gentoo
+install_file gentoo root root    644 '/etc/ccache.conf'
+install_file gentoo root root    644 '/etc/dracut.conf'
 echo
-install_file root root    644 '/etc/conf.d/display-manager'               gentoo
-install_file root root    644 '/etc/default/grub'                         gentoo
-install_file root root    644 '/etc/env.d/70less'                         gentoo
-install_file root root    644 '/etc/env.d/90xsession'                     gentoo
-install_file root root    644 '/etc/pam.d/lightdm'                        gentoo
-install_file root portage 644 '/var/lib/portage/world'                    gentoo
+install_file gentoo root root    644 '/etc/conf.d/display-manager'
+install_file gentoo root root    644 '/etc/default/grub'
+install_file gentoo root root    644 '/etc/env.d/70less'
+install_file gentoo root root    644 '/etc/env.d/90xsession'
+install_file gentoo root root    644 '/etc/pam.d/lightdm'
+install_file gentoo root portage 644 '/var/lib/portage/world'
 echo
-install_dir  root root    755 '/etc/portage/env'
-install_dir  root root    755 '/etc/portage/package.accept_keywords'
-install_dir  root root    755 '/etc/portage/package.env'
-install_dir  root root    755 '/etc/portage/package.license'
-install_dir  root root    755 '/etc/portage/package.mask'
-install_dir  root root    755 '/etc/portage/package.use'
-install_dir  root root    755 '/etc/portage/repos.conf'
-install_file root root    644 '/etc/portage/env/ccache.conf'              gentoo
-install_file root root    644 '/etc/portage/make.conf'                    gentoo
-install_file root root    644 '/etc/portage/package.accept_keywords/misc' gentoo
-install_file root root    644 '/etc/portage/package.accept_keywords/netsurf' gentoo
-install_file root root    644 '/etc/portage/package.env/ccache'           gentoo
-install_file root root    644 '/etc/portage/package.license/firmware'     gentoo
-install_file root root    644 '/etc/portage/package.license/media'        gentoo
-install_file root root    644 '/etc/portage/package.license/misc'         gentoo
-install_file root root    644 '/etc/portage/package.mask/binsrc'          gentoo
-install_file root root    644 '/etc/portage/package.mask/bloat'           gentoo
-install_file root root    644 '/etc/portage/package.mask/noupgrade'       gentoo
-install_file root root    644 '/etc/portage/package.use/desktop'          gentoo
-install_file root root    644 '/etc/portage/package.use/heavy_clients'    gentoo
-install_file root root    644 '/etc/portage/package.use/kernel'           gentoo
-install_file root root    644 '/etc/portage/package.use/toolchains'       gentoo
-install_file root root    644 '/etc/portage/package.use/zz-autounmask'    gentoo
-install_file root root    644 '/etc/portage/repos.conf/gentoo.conf'       gentoo
+install_dir         root root    755 '/etc/portage/env'
+install_dir         root root    755 '/etc/portage/package.accept_keywords'
+install_dir         root root    755 '/etc/portage/package.env'
+install_dir         root root    755 '/etc/portage/package.license'
+install_dir         root root    755 '/etc/portage/package.mask'
+install_dir         root root    755 '/etc/portage/package.use'
+install_dir         root root    755 '/etc/portage/repos.conf'
+install_file gentoo root root    644 '/etc/portage/env/ccache.conf'
+install_file gentoo root root    644 '/etc/portage/make.conf'
+install_file gentoo root root    644 '/etc/portage/package.accept_keywords/misc'
+install_file gentoo root root    644 '/etc/portage/package.accept_keywords/netsurf'
+install_file gentoo root root    644 '/etc/portage/package.env/ccache'
+install_file gentoo root root    644 '/etc/portage/package.license/firmware'
+install_file gentoo root root    644 '/etc/portage/package.license/media'
+install_file gentoo root root    644 '/etc/portage/package.license/misc'
+install_file gentoo root root    644 '/etc/portage/package.mask/binsrc'
+install_file gentoo root root    644 '/etc/portage/package.mask/bloat'
+install_file gentoo root root    644 '/etc/portage/package.mask/noupgrade'
+install_file gentoo root root    644 '/etc/portage/package.use/desktop'
+install_file gentoo root root    644 '/etc/portage/package.use/heavy_clients'
+install_file gentoo root root    644 '/etc/portage/package.use/kernel'
+install_file gentoo root root    644 '/etc/portage/package.use/toolchains'
+install_file gentoo root root    644 '/etc/portage/package.use/zz-autounmask'
+install_file gentoo root root    644 '/etc/portage/repos.conf/gentoo.conf'
 fi
