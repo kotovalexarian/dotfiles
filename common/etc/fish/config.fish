0253c33c5eec07161f17757089d2dd95912f9939
Alex Kotov <git@kotovalexarian.com>
2024-10-27 12:37:58 +0400

    fish: improve aliases

--- common/etc/fish/config.fish
+++ common/etc/fish/config.fish
@@ -2,9 +2,12 @@ set fish_color_host brgreen
 set fish_greeting ''
 set fish_prompt_pwd_dir_length 2
 
-alias c='clear'
+alias c 'clear'
 
 alias ll  'ls -lhTF'
 alias lal 'ls -lhTFA'
 alias laa 'ls -lhTFa'
 alias ldl 'ls -lhTFd'
+
+alias df 'df -h'
+alias du 'du -h'
