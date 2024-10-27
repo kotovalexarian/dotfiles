8ac739a4738f5d7675bc625e9456e9a436c60244
Alex Kotov <git@kotovalexarian.com>
2024-10-27 11:08:16 +0400

    fish: improve aliases

--- common/etc/fish/config.fish
+++ common/etc/fish/config.fish
@@ -3,5 +3,8 @@ set fish_greeting ''
 set fish_prompt_pwd_dir_length 2
 
 alias c='clear'
-alias lal='ls -alF'
-alias ll='ls -lF'
+
+alias ll  'ls -lhTF'
+alias lal 'ls -lhTFA'
+alias laa 'ls -lhTFa'
+alias ldl 'ls -lhTFd'
