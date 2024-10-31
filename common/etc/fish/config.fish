648fcf651bd7df445af4f3d756be61950c279241
Alex Kotov <git@kotovalexarian.com>
2024-10-31 08:29:20 +0400

    fish: aliases

--- common/etc/fish/config.fish
+++ common/etc/fish/config.fish
@@ -5,8 +5,8 @@ set fish_prompt_pwd_dir_length 2
 alias c 'clear'
 
 alias ll  'ls -lhTF'
-alias lal 'ls -lhTFA'
-alias laa 'ls -lhTFa'
+alias la  'ls -lhTFA'
+alias lal 'ls -lhTFa'
 alias ldl 'ls -lhTFd'
 
 alias df 'df -h'
